461b25145001697f89959cba30856f25572eef73
Dan Gilleland <dgilleland@nait.ca>
2025-11-24 20:05:09 -0700

    Intro to setInterval()

--- src/014/IntroToIntervals/js/main.js
+++ src/014/IntroToIntervals/js/main.js
@@ -4,8 +4,24 @@ const addDots = function() {
     myOutput.setAttribute('title', `There are ${myOutput.innerText.length} dots`);
 }
 
+// Start an interval
+let cancellationRef = setInterval(addDots, 1250); // 1.25 seconds
+
 // Add an event listener to the button that will stop the interval.
 document.querySelector('button')
         .addEventListener('click', function(evt) {
-            console.log('Button clicked');
+            console.log('Button clicked', cancellationRef);
+            // Any value that is false, null, undefined, "", or 0
+            // is considered "falsy".
+            // Anything else is considered "truthy"
+            if(cancellationRef) {   // if "truthy"
+                clearInterval(cancellationRef);
+                cancellationRef = 0; // to indicate the interval is no
+                                     // longer running.
+                evt.target.innerText = "Continue Interval";
+            } else {
+                // Re-starting means creating a new interval/timer
+                cancellationRef = setInterval(addDots, 1250);
+                evt.target.innerText = "Stop Interval";
+            }
         });
