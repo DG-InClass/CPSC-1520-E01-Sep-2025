6b5daab9092c2e7393245f1bd2a1cad29e5df45e
Dan Gilleland <dgilleland@nait.ca>
2025-11-24 19:35:21 -0700

    Intro to setTimeout() function

--- src/014/IntroToTimeouts/js/main.js
+++ src/014/IntroToTimeouts/js/main.js
@@ -1,5 +1,14 @@
 // TODO: Add code here
-
+const buttons = document.querySelectorAll('input[type=button]');
+buttons.forEach(btn => {
+    const ms = parseInt(btn.dataset.ms); // <input ... data-ms="1000"
+    // Call setTimeout() to wait a certain amount of time, and then
+    // disable this button.
+    const timerRef = setTimeout(() => {
+        btn.disabled = true;
+    }, ms); // don't forget the second argument - # of milliseconds
+    btn.dataset.timer = timerRef; // remember the reference to a timer
+});
 
 const output = document.getElementById('total');
 
@@ -8,8 +17,9 @@ document.querySelector('main > section').addEventListener('click', (evt) => {
         /** @type {HTMLInputElement} */
         const btn = evt.target;
         console.log('clicked');
-        if(!btn.dataset.clicked) {
+        if(!btn.dataset.clicked) { // <input type="button" data-clicked
             btn.dataset.clicked = true; // Give the prize once
+            clearTimeout(btn.dataset.timer); // Stop the timer
 
             btn.classList.add('text-green-700');
             const amount = parseInt(btn.value);
