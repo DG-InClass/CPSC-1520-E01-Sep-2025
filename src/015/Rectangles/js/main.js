2f4ba3ecf239c6f0b47718e01edfa597d4197802
Dan Gilleland <dgilleland@nait.ca>
2025-11-27 09:29:33 -0700

    Render rectangles

--- src/015/Rectangles/js/main.js
+++ src/015/Rectangles/js/main.js
@@ -26,7 +26,39 @@ const Rectangle = class {
     report() {
         console.log(`This box was made on ${this.#created.toLocaleTimeString()}`);
     }
-    
+
+    /**
+     * Render this rectangle into the DOM.
+     * @param {HTMLElement} container The DOM element to hold this shape
+     * @returns {HTMLDivElement} - This rectangle as a `<div>`
+     */
+    render(container) {
+        // Create a `<div>` with certain styles
+        // - w-[{width}px]
+        // - h-[{height}px]
+        const divEl = document.createElement('div');
+
+        // NOTE: Attempting to generate Tailwind classes via
+        //       string interpolation will NOT work, because
+        //       Tailwind cannot predict what the height and
+        //       width values might be.
+        //       So, do NOT use the approach in these lines 🐞
+        // const customWidth = `w-[${this.width}px]`;
+        // const customHeight = `h-[${this.height}px]`;
+        // divEl.classList.add(customWidth, customHeight);
+
+        // NOTE: Instead, apply the styles as plain CSS to
+        //       the divEl element. 🎉
+        divEl.style.width = `${this.height}px`;
+        divEl.style.height = `${this.width}px`;
+        
+
+        // Append the div to the container.
+        container.appendChild(divEl);
+
+        // Return a reference
+        return divEl;
+    }
 
     // Static member of the Rectangle class
     static #count = 0; // Private, static property/variable
@@ -41,12 +73,15 @@ const shapes = [];
 
 document.querySelector('button').addEventListener('click', (evt) => {
     evt.preventDefault();
-    const w = 10;
-    const h = 5;
+    const w = 100;
+    const h = 55;
     
     // Create the Rectangle object.
     const box = new Rectangle(h, w); // Instantiate the class
-    
+
+    const domContainer = document.getElementById('shapes');
+    box.render(domContainer);
+
     shapes.push(box);
     console.log('box created:', box);
     console.log(`There are ${Rectangle.count} boxes so far.`);
