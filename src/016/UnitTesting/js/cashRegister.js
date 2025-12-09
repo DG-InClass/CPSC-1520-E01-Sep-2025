37f0af11a542a58463197c9020fefbe6724399c4
Dan Gilleland <dgilleland@nait.ca>
2025-12-08 21:45:58 -0700

    Refactoring stage of TDD Red-Green-Refactor

--- src/016/UnitTesting/js/cashRegister.js
+++ src/016/UnitTesting/js/cashRegister.js
@@ -1,9 +1,15 @@
 // js/cashRegister.js
 
-export function acceptPayment(paymentInfo) {
-    // destructuring syntax is an easy way to "unpack" an object   
-    let { total, tenderedAmount, paymentType } = paymentInfo;
-
+/**
+ * acceptPayment() processes a cash register payment.
+ * 
+ * @param {Object} paymentInfo Payment Information
+ * @param {number} paymentInfo.total The total amount of the purchase
+ * @param {number} paymentInfo.tenderedAmount The amount tendered
+ * @param {string} paymentInfo.paymentType The payment method
+ * @returns {{total: number, change: number, tenderedAmount: number paymentType: string, status: string }} - the receipt for the payment
+ */
+export function acceptPayment({ total, tenderedAmount, paymentType }) {
     let result = {
         total: total, // or I could have simplified with `total,`
         change: tenderedAmount - total,
@@ -12,7 +18,6 @@ export function acceptPayment(paymentInfo) {
     };
     // Actual calculations...
     result.status = 'PAID';
-
 
 
     return result;
